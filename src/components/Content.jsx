ba3c2ac5d8fc32d1a8f33d929c2d70c97b56e49d
yayacopernic23 <yayakomara56@gmail.com>
2026-01-25 17:08:15 +0100

    Mise en place du contenu

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,3 +1,18 @@
+import styles from "./Content.module.scss";
 export default function Content() {
-  return <div className="flex-fill b3"> Content </div>;
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30 ">Découvrez nos nouvelles recettes</h1>
+      <div className={` ${styles.contentCard} card p-10`}>
+        <div className={`${styles.grid} `}>
+          <div className={styles.elem}>ELEMENT</div>
+          <div className={styles.elem}>ELEMENT</div>
+          <div className={styles.elem}>ELEMENT</div>
+          <div className={styles.elem}>ELEMENT</div>
+          <div className={styles.elem}>ELEMENT</div>
+          <div className={styles.elem}>ELEMENT</div>
+        </div>
+      </div>
+    </div>
+  );
 }
