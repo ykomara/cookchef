01633ffbf95bc3d61ab1d737fb60fe0db7558421
yayacopernic23 <yayakomara56@gmail.com>
2026-01-29 22:54:43 +0100

    Rendre l'application responsive avec les mixins sass

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,7 +6,7 @@ export default function Content() {
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30 ">Découvrez nos nouvelles recettes</h1>
-      <div className={` ${styles.contentCard} card p-10`}>
+      <div className={` ${styles.contentCard} card p-10 br`}>
         <div className={`${styles.grid} `}>
           {recipes.map((r) => (
             <Recipe key={r._id} title={r.title} image={r.image} />
